319eeb4ee122efb0d7dc5b3c3808ebbaa18bdd78
lee-leonardo <leoleez@hotmail.com>
2014-08-11 14:33:04 -0700

    Initialized Enso project

---
+++ Enso/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Enso
+//
+//  Created by Leonardo Lee on 8/11/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+//MARK: UIPageViewControllerDataSource
+	
+	
+//MARK: View methods
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+
+
+}
+
