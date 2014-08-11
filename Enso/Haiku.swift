8296463a4154b5491ba3f2d0784bc6855c1e104d
lee-leonardo <leoleez@hotmail.com>
2014-08-11 16:38:15 -0700

    Implemented the boilerplate. I need to create cases for the specific ones I guess, and make the Pages a bit more robust.

---
+++ Enso/Haiku.swift
@@ -0,0 +1,23 @@
+//
+//  Haiku.swift
+//  Enso
+//
+//  Created by Leonardo Lee on 8/11/14.
+//  Copyright (c) 2014 Leonardo Lee. All rights reserved.
+//
+
+import Foundation
+
+class Haiku {
+	var text : String
+	
+	init(haiku text: String) {
+		self.text = text
+	}
+	
+	//Algorithm used here is attributed to Franklin Mark Liang of Stanford University
+	func countSyllables() -> Int {
+		
+		return 0
+	}
+}
