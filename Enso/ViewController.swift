e5af944a27792e56b8af07a4997d70390127b533
lee-leonardo <leoleez@hotmail.com>
2014-08-11 20:22:40 -0700

    UIPageViewController turned out to be unintuitive and nearly usable only for tutorials by design. So I'm switching to UITabBarController.

--- Enso/ViewController.swift
+++ Enso/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPageViewControllerDataSource {
+class ViewController: UIViewController {
 	
 //Variables?
 	//PHAsset
@@ -16,67 +16,11 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 
 	
 //MARK: UIPageViewController - Variables
-	var pageViewController = UIPageViewController()
 	var preferenceController = PreferenceViewController()
 	var haikuController = HaikuViewController()
 	var photoController = PhotoViewController()
 	
-	var pages: [UIViewController] {
-		return [preferenceController, haikuController, photoController]
-	}
 	
-//	enum PageControllerIndex {
-//		case preference = 0, haiku, photo
-//	}
-	
-	func viewControllerAtIndex(index: Int) -> UIViewController! {
-		if self.pages.count == 0 || index >= self.pages.count {
-			return nil
-		}
-		
-		var pageViewController = self.storyboard.instantiateInitialViewController().pages[index]
-		
-		return pageViewController
-	}
-	
-	
-//MARK: UIPageViewController - DataSource
-	func pageViewController(pageViewController: UIPageViewController!, viewControllerBeforeViewController viewController: UIViewController!) -> UIViewController! {
-		
-		var presentedController = viewController as BasePageViewController
-		var index = presentedController.pageIndex
-		
-		if index == 0 || index == nil {
-			return nil
-		}
-		
-		index = index! - 1
-		return viewControllerAtIndex(index!)
-		
-	}
-	func pageViewController(pageViewController: UIPageViewController!, viewControllerAfterViewController viewController: UIViewController!) -> UIViewController! {
-		
-		var presentedController = viewController as BasePageViewController
-		var index = presentedController.pageIndex
-		
-		if index == nil {
-			return nil
-		}
-		index = index! + 1
-		
-		if index == self.pages.count {
-			return nil
-		}
-		
-		return viewControllerAtIndex(index!)
-		
-	}
-	func presentationCountForPageViewController(pageViewController: UIPageViewController!) -> Int {
-		return self.pages.count
-	}
-	func presentationIndexForPageViewController(pageViewController: UIPageViewController!) -> Int {
-		return 1
-	}
 	
 //MARK: View methods
 	override func viewWillAppear(animated: Bool) {
@@ -84,18 +28,6 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		self.pageViewController = self.storyboard.instantiateViewControllerWithIdentifier("PageViewController") as UIPageViewController
-		self.pageViewController.dataSource = self
-		
-		var startingViewController = self.viewControllerAtIndex(1) as HaikuViewController
-		var viewControllers = [startingViewController] as NSArray
-		self.pageViewController.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
-		
-		self.pageViewController.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 30)
-		self.addChildViewController(pageViewController)
-		self.view.addSubview(pageViewController.view)
-		self.pageViewController.didMoveToParentViewController(self)
 		
 	}
 	override func didReceiveMemoryWarning() {
