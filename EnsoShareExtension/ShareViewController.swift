8776f649a861568c4924fa332107f65784c298c2
lee-leonardo <leoleez@hotmail.com>
2014-08-19 22:58:18 -0700

    Implemented the basics of a Share Extension. It is going to take a while for me to understand how to do it. It seems like I'll need to learn more about grand central dispatch and SLComposeViewControllers.

--- EnsoShareExtension/ShareViewController.swift
+++ EnsoShareExtension/ShareViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 import Social
 
 class ShareViewController: SLComposeServiceViewController {
+	
+	var imageData = NSData()
+	var haikuText : NSString = "Wow!"
 
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
+		
+		
+		var message = self.contentText.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) as NSString
+		var messageLength = message.length
+		
+		var remainingCharacters = 100 - messageLength
+		self.charactersRemaining = remainingCharacters
+		
+		if charactersRemaining >= 0 {
+			return true
+		}
+		//return false
+		
+		
+		
+		
+		
+		
+		
+		
+		self.charactersRemaining = haikuText.length
+		
+		if self.charactersRemaining > 140 || imageData == nil {
+			return false
+		}
+		
         return true
     }
-
+	
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
+
+//		var inputItem = self.extensionContext.inputItems.first as NSExtensionItem
+//		var outputItem = inputItem.copy() as NSExtensionItem
+//		outputItem.attributedContentText = NSAttributedString(string: self.contentText)
+//		var outputItems = [outputItem]
+//		self.extensionContext.completeRequestReturningItems(outputItems, completionHandler: nil)
+		
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+		
         self.extensionContext.completeRequestReturningItems(nil, completionHandler: nil)
     }
 
@@ -27,5 +63,14 @@ class ShareViewController: SLComposeServiceViewController {
         // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
         return NSArray()
     }
-
+	
+	
+	//Upload
+	func performUpload() {
+		
+//		var configurationName = "com.shareSheet.enso.background-configuration"
+//		var configuration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier(configurationName)
+//		var session = NSURLSession.sessionWithConfiguration(configuration, delegate: self, delegateQueue: <#NSOperationQueue!#>)
+	}
+	
 }
